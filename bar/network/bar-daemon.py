637120c3cd85e3530eb8b0769fbfdec7fcc8e96c
George Chatzisofroniou <sophron@latthi.com>
2014-03-16 01:46:41 +0200

    Daemon should also close connection.

--- bar/network/bar-daemon.py
+++ bar/network/bar-daemon.py
@@ -3,6 +3,8 @@ import os
 import psutil
 import time
 import zlib
+import sys
+import sqlite3 as lite
 from twisted.internet.protocol import ServerFactory, ClientFactory, Protocol
 from twisted.protocols import basic
 from twisted.python import log
@@ -15,28 +17,18 @@ class CommunicatorProtocol(Protocol):
         if data[:2] == "OK":
             print "Received a succesfull message from the server."
             self.factory.listener_factory.send_message(data)
-
         splitdata = data.split("|||")
         label = splitdata[0]
-
-        import sqlite3 as lite
-        import sys
-
         con = lite.connect('bar/db/bar.db')
-
-        with con:                
+        with con:
             cur = con.cursor() 
             cur.execute("SELECT * FROM contacts WHERE label=:label", {"label":label})
-
             row = cur.fetchone()
-
             if row != None:
-
                 start = time.time()
                 encrypted = splitdata[1]
                 cleartext = self.AESdecrypt(row[4], encrypted)
                 newlabel = cleartext.split("|||")[0]
-
                 cur.execute("INSERT INTO messages(message) VALUES(?);", (cleartext.split("|||")[1].decode('ascii'),))
                 cur.execute("UPDATE contacts SET label=? WHERE label=?", (newlabel, label))
             else:
@@ -46,7 +38,6 @@ class CommunicatorProtocol(Protocol):
         '''
         Decrypt given message with shared key.
         '''
-
         iv = '\x00' * 16
         stream=AES.new(skey, AES.MODE_CFB, iv)
         return stream.decrypt(c)
@@ -73,6 +64,7 @@ class ListenerProtocol(Protocol):
 
     def dataReceived(self, data):
         self.factory.communicator_factory.send_message(data)
+        self.transport.loseConnection()
 
     def connectionMade(self):
         self.factory.clientConnectionMade(self)
@@ -96,12 +88,9 @@ def main():
     communicator_factory.set_listener(listener_factory)
     print "Starting reactor..."
     reactor.connectTCP("localhost", 231, communicator_factory)
-
     port = reactor.listenTCP(4333, listener_factory,
                              interface="127.0.0.1")
-
     print 'Listening on %s.' % (port.getHost())
-
     reactor.run()
 
 if __name__ == '__main__':
