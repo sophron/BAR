9887e3434310456e0efec0e94d262940f7b242e3
George Chatzisofroniou <sophron@latthi.com>
2014-05-15 16:39:26 +0300

    Make use of HTTP proxy.

--- bar/network/bar-daemon.py
+++ bar/network/bar-daemon.py
@@ -10,6 +10,7 @@ import sqlite3 as lite
 from twisted.internet.protocol import ServerFactory, ClientFactory, Protocol
 from twisted.protocols.basic import LineReceiver, NetstringReceiver
 from twisted.protocols import basic
+from twisted.web import proxy, http
 from twisted.python import log
 from twisted.internet import reactor
 from bar.pybar import send_message
@@ -47,26 +48,27 @@ class CommunicatorProtocol(NetstringReceiver):
         return 1
 
     def received_broadcast(self, data):
+            print "Received feed."
             message = Message(data)
             row = db.select_entry("label", message.label)
+            if not row:
+                print "Can't find this label: " + message.label
             message.decrypt(row[4])
             if not message.validate():
                 print "Received an invalid message."
                 return
+            print "Found label: " + message.label
             if HIDDEN_CLIENT:
                 if self.factory.listener_factory:
                     self.factory.listener_factory.send_message(message.cleartext_msg)
                     self.factory.listener_factory.client.transport.loseConnection()
-                    db.update_entry("label", message.label, "label", message.new_label)
+                    #db.update_entry("label", message.label, "label", message.new_label)
+                    db.insert_entry("foo6", message.new_label, row[4])
             if HIDDEN_SERVICE:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect(('localhost', HIDDEN_SERVICE_PORT))
-                s.send(message.cleartext_msg)
-                data = s.recv(2048)
-                s.close()
-                more_new_label = label.gen_lbl()
-                db.update_entry("label", retr_label, "label", more_new_label)
-                self.factory.send_broadcast_request(message.new_label, row[4], more_new_label, data)
+                socks_client_factory = HTTPClientFactory(reactor, message)
+                socks_client_factory.set_communicator(self)
+                reactor.connectTCP("localhost", 4333, socks_client_factory)
+                db.insert_entry("foo6", message.new_label, row[4])
 
     def stringReceived(self, data):
         for op in self.operations:
@@ -100,20 +102,21 @@ class CommunicatorFactory(ClientFactory):
                          + "|||" + str(newlabel) \
                          + "|||" + str(message)))
 
-class ListenerProtocol(NetstringReceiver):
+class ListenerProtocol(Protocol):
 
     def dataReceived(self, data):
-        host = re.findall(r"Host: (?P<value>.*?)\r\n", data)[0]
-        contact = db.select_entry("name", host)
+        contact = db.select_entry("name", "foo6")
         newlabel = label.gen_lbl()
         if contact:
             self.factory.communicator_factory.send_broadcast_request(contact[2], contact[4], newlabel, data)
-            db.update_entry("label", contact[2], "label", newlabel)
+        return
 
     def connectionMade(self):
         self.factory.clientConnectionMade(self)
 
+
 class ListenerFactory(ServerFactory):
+
     protocol = ListenerProtocol
 
     def __init__(self, reactor, communicator_factory):
@@ -126,14 +129,45 @@ class ListenerFactory(ServerFactory):
     def send_message(self, msg):
         self.client.transport.write(msg)
 
+class HTTPClientProtocol(Protocol):
+
+    def dataReceived(self, data):
+        contact = db.select_entry("name", "foo6")
+        more_new_label = label.gen_lbl()
+        self.factory.communicator_factory.factory.send_broadcast_request(self.factory.message.label, contact[4], more_new_label, data)
+
+    def connectionMade(self):
+        self.factory.clientConnectionMade(self)
+
+class HTTPClientFactory(ClientFactory):
+    protocol = HTTPClientProtocol
+
+    def __init__(self, reactor, message):
+        self.reactor = reactor
+        self.message = message
+
+    def clientConnectionMade(self, client):
+        self.client = client
+        self.client.transport.write(self.message.cleartext_msg)
+
+    def set_communicator(self, communicator_factory):
+        self.communicator_factory = communicator_factory
+
+class ProxyFactory(http.HTTPFactory):
+	protocol = proxy.Proxy
+
 def main():
     communicator_factory = CommunicatorFactory(reactor)
-    listener_factory = ListenerFactory(reactor, communicator_factory)
-    communicator_factory.set_listener(listener_factory)
+    if HIDDEN_CLIENT: 
+        listener_factory = ListenerFactory(reactor, communicator_factory)
+        communicator_factory.set_listener(listener_factory)
+        port = reactor.listenTCP(4333, listener_factory,
+                                 interface="127.0.0.1")
+    elif HIDDEN_SERVICE:
+        port = reactor.listenTCP(4333, ProxyFactory())
+
     print "Starting reactor..."
     reactor.connectTCP("192.168.1.2", 231, communicator_factory)
-    port = reactor.listenTCP(4333, listener_factory,
-                             interface="127.0.0.1")
     print 'Listening on %s.' % (port.getHost())
     reactor.run()
 
